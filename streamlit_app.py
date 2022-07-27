2c63766b912b6e75cf9b20fff357f83350d927c6
Fran Muñoz <108275294+franmunozsoria@users.noreply.github.com>
2022-07-27 15:24:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title('Hola amigo')
 streamlit.header('Header')
 streamlit.text('Text')
 
-streamlit.text('🥣 🥗 🐔 🥑🍞'
+streamlit.text('🥣 🥗 🐔 🥑🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
                
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
